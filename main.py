f08b8127c431700e7970d894b6e39c4f69e08598
Taylor Bollman <taylor@voicelayer.io>
2023-10-11 17:39:38 -0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,42 @@
+
+from icecream import ic
+import numpy as np
+import torch
+from transformers import AutoTokenizer, AutoModelForSequenceClassification, DataCollatorWithPadding
+from transformers import TrainingArguments, Trainer
+import accelerate
+import huggingface_hub
+from datasets import load_dataset
+
+
+
+
+checkpoint = "bert-base-uncased"
+tokenizer = AutoTokenizer.from_pretrained(checkpoint)
+model = AutoModelForSequenceClassification.from_pretrained(checkpoint, num_labels=2)
+data_collator = DataCollatorWithPadding(tokenizer=tokenizer)
+training_args = TrainingArguments("test-trainer")
+raw_dataset = load_dataset("glue", "mrpc")
+
+
+tokenized_dataset = raw_dataset.map(
+    lambda examples: tokenizer(examples["sentence1"], examples["sentence2"], truncation=True, padding=True),
+    batched=True,
+    num_proc=4,
+)
+
+trainer = Trainer(
+    model=model,
+    args=training_args,
+    train_dataset=tokenized_dataset["train"],
+    data_collator=data_collator,
+)
+trainer.train()
+
+
+
+# trainer = Trainer(model=model, args=training_args, 
+#                   train_dataset=tokenized_dataset["train"], 
+#                   data_collator=data_collator, tokenizer=tokenizer)
+# trainer.train()
+

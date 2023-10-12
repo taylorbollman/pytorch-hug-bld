01caf49bb144e0a2bdea2d44e98d914a6b879de8
Taylor Bollman <taylor.bollman@gmail.com>
2023-10-11 17:43:10 -0700

    dfsdf

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 
-from icecream import ic
+# from icecream import ic
 import numpy as np
+
 import torch
 from transformers import AutoTokenizer, AutoModelForSequenceClassification, DataCollatorWithPadding
 from transformers import TrainingArguments, Trainer
@@ -9,29 +10,45 @@ import huggingface_hub
 from datasets import load_dataset
 
 
+print('heelllllo there!')
 
 
-checkpoint = "bert-base-uncased"
-tokenizer = AutoTokenizer.from_pretrained(checkpoint)
-model = AutoModelForSequenceClassification.from_pretrained(checkpoint, num_labels=2)
-data_collator = DataCollatorWithPadding(tokenizer=tokenizer)
-training_args = TrainingArguments("test-trainer")
-raw_dataset = load_dataset("glue", "mrpc")
+# checkpoint = "bert-base-uncased"
+# tokenizer = AutoTokenizer.from_pretrained(checkpoint)
+# model = AutoModelForSequenceClassification.from_pretrained(checkpoint, num_labels=2)
+# data_collator = DataCollatorWithPadding(tokenizer=tokenizer)
+# training_args = TrainingArguments("test-trainer")
+# raw_dataset = load_dataset("glue", "mrpc")
 
 
-tokenized_dataset = raw_dataset.map(
-    lambda examples: tokenizer(examples["sentence1"], examples["sentence2"], truncation=True, padding=True),
-    batched=True,
-    num_proc=4,
-)
+# tokenized_dataset = raw_dataset.map(
+#     lambda examples: tokenizer(examples["sentence1"], examples["sentence2"], truncation=True, padding=True),
+#     batched=True,
+#     num_proc=4,
+# )
 
-trainer = Trainer(
-    model=model,
-    args=training_args,
-    train_dataset=tokenized_dataset["train"],
-    data_collator=data_collator,
-)
-trainer.train()
+# trainer = Trainer(
+#     model=model,
+#     args=training_args,
+#     train_dataset=tokenized_dataset["train"],
+#     data_collator=data_collator,
+# )
+# trainer.train()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
